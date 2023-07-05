a4775438c8a56c56117671d3c0d18e55675e050e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2023-07-05 09:58:41 +0200

    cooja: remove home-grown thread routines
    Switch to the POSIX.1-2001 setcontext/getcontext
    routines instead of hand-written assembly.
    These routines were removed in POSIX.1-2008 for
    portability reasons with the recommendation
    to use pthreads instead, but that is not feasible
    for Cooja.
    
    This patch should fix the M1/M2 Mac support for
    Cooja.

--- arch/platform/cooja/sys/cooja_mt.c
+++ arch/platform/cooja/sys/cooja_mt.c
@@ -36,36 +36,87 @@
  * Fredrik Osterlind <fros@sics.se>
  */
 
+#define _GNU_SOURCE /* For pthread_getattr_np(). */
+#include <pthread.h>
+
 #include "contiki.h"
 #include "sys/cooja_mt.h"
 #include "sys/cc.h"
+
+#include <signal.h>
+#include <stdio.h>
 
 #define MT_STATE_READY   1
 #define MT_STATE_RUNNING 2
 #define MT_STATE_EXITED  5
 
 static struct cooja_mt_thread *current;
+static struct cooja_mt_thread *cooja_running_thread;
 
 /*--------------------------------------------------------------------------*/
-void
-cooja_mt_start(struct cooja_mt_thread *thread, void (* function)(void *), void *data)
+int
+cooja_mt_init(struct cooja_mt_thread *t)
 {
-  /* Call the architecture dependant function to set up the processor
-     stack with the correct parameters. */
-  cooja_mtarch_start(&thread->thread, function, data);
-
-  thread->state = MT_STATE_READY;
+#ifdef __APPLE__
+  /* Apple's makecontext() fails if size is less than MINSIGSTKSZ. */
+  if(COOJA_MTARCH_STACKSIZE < MINSIGSTKSZ) {
+    return -1;
+  }
+#endif
+  void *stackAddr;
+  size_t stackSize;
+  pthread_t self = pthread_self();
+#ifdef __APPLE__
+  stackAddr = pthread_get_stackaddr_np(self);
+  stackSize = pthread_get_stacksize_np(self);
+#else
+  pthread_attr_t attrs;
+  if(pthread_getattr_np(self, &attrs) != 0) {
+    return -2;
+  }
+  if(pthread_attr_getstack(&attrs, &stackAddr, &stackSize) != 0) {
+    return -3;
+  }
+#endif
+  getcontext(&t->ctxt);
+  t->ctxt.uc_stack.ss_sp = stackAddr;
+  t->ctxt.uc_stack.ss_flags = 0;
+  t->ctxt.uc_stack.ss_size = stackSize;
+  return 0;
 }
 /*--------------------------------------------------------------------------*/
 void
-cooja_mt_exec(struct cooja_mt_thread *thread)
+cooja_mt_start(struct cooja_mt_thread *caller,
+               struct cooja_mt_thread *t, void (*function)(void))
+{
+  for(int i = 0; i < COOJA_MTARCH_STACKSIZE; ++i) {
+    t->stack[i] = i;
+  }
+
+  if(getcontext(&t->ctxt) == -1) {
+    perror("getcontext failed");
+  }
+  t->ctxt.uc_stack.ss_sp = t->stack;
+  t->ctxt.uc_stack.ss_flags = 0;
+  t->ctxt.uc_stack.ss_size = sizeof(t->stack);
+  t->ctxt.uc_link = &caller->ctxt;
+  makecontext(&t->ctxt, function, 0);
+  t->state = MT_STATE_READY;
+}
+/*--------------------------------------------------------------------------*/
+void
+cooja_mt_exec(struct cooja_mt_thread *caller, struct cooja_mt_thread *thread)
 {
   if(thread->state == MT_STATE_READY) {
     thread->state = MT_STATE_RUNNING;
     current = thread;
     /* Switch context to the thread. The function call will not return
        until the the thread has yielded, or is preempted. */
-    cooja_mtarch_exec(&thread->thread);
+    cooja_running_thread = thread;
+    if(swapcontext(&caller->ctxt, &thread->ctxt) == -1) {
+      perror("swapcontext");
+    }
+    cooja_running_thread = NULL;
   }
 }
 /*--------------------------------------------------------------------------*/
@@ -78,5 +129,11 @@ cooja_mt_yield(void)
      switch function in order to switch the thread to the main Contiki
      program instead. For us, the switch function will not return
      until the next time we are scheduled to run. */
-  cooja_mtarch_yield();
+  if(cooja_running_thread->ctxt.uc_link == NULL) {
+    return;
+  }
+  if(swapcontext(&cooja_running_thread->ctxt,
+                 cooja_running_thread->ctxt.uc_link) == -1) {
+    perror("swapcontext");
+  }
 }
