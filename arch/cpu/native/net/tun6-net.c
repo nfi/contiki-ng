ad5f79d384fa9ab4019a4213bff35a54f85e172d
Niclas Finne <niclas.finne@ri.se>
2024-02-29 13:57:51 +0100

    native: add option to save communication to pcap file

--- arch/cpu/native/net/tun6-net.c
+++ arch/cpu/native/net/tun6-net.c
@@ -35,6 +35,7 @@
 
 #include "net/ipv6/uip.h"
 #include "net/ipv6/uip-ds6.h"
+#include "lib/crc32.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -50,6 +51,7 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <net/if.h>
+#include "sys/platform.h"
 
 /* Log configuration */
 #include "sys/log.h"
@@ -65,10 +67,16 @@
 #include "net/netstack.h"
 #include "net/packetbuf.h"
 
+static FILE *open_pcap(const char *filename);
+static void write_pcap_packet(FILE *pcap_file);
+static void close_pcap(FILE *pcap_file);
+
+static const char *pcap_output_file;
+static FILE *pcap_file;
+
 static const char *config_ipaddr = "fd00::1/64";
 /* Allocate some bytes in RAM and copy the string */
 static char config_tundev[IFNAMSIZ + 1] = "tun0";
-
 
 static int tunfd = -1;
 
@@ -118,6 +126,11 @@ cleanup(void)
   strncat(buf, "\") print \"route delete -net \"$1; }'"
           " | sh", TMPBUFSIZE - strlen(buf) - 1);
   system(buf);
+
+  if(pcap_file) {
+    close_pcap(pcap_file);
+    pcap_file = NULL;
+  }
 }
 
 /*---------------------------------------------------------------------------*/
@@ -208,11 +221,23 @@ tun_init()
 {
   setvbuf(stdout, NULL, _IOLBF, 0); /* Line buffered output. */
 
+  if(pcap_output_file) {
+    pcap_file = open_pcap(pcap_output_file);
+    if(pcap_file == NULL) {
+      fprintf(stderr, "Warning: failed to open PCAP file '%s' for writing!\n",
+              pcap_output_file);
+    } else {
+      fprintf(stderr, "Saving PCAP to %s\n", pcap_output_file);
+    }
+  }
+
   LOG_INFO("Initializing tun interface\n");
 
   tunfd = tun_alloc(config_tundev, sizeof(config_tundev));
   if(tunfd == -1) {
-    LOG_WARN("Failed to open tun device (you may be lacking permission). Running without network.\n");
+    LOG_WARN("Failed to open tun device '%s' "
+             "(you may be lacking permission). Running without network.\n",
+             config_tundev);
     /* err(1, "failed to allocate tun device ``%s''", config_tundev); */
     return;
   }
@@ -264,6 +289,9 @@ output(const linkaddr_t *localdest)
 {
   LOG_DBG("SUT: %u\n", uip_len);
   if(uip_len > 0) {
+    if(pcap_file) {
+      write_pcap_packet(pcap_file);
+    }
     return tun_output(uip_buf, uip_len);
   }
   return 0;
@@ -295,13 +323,16 @@ handle_fd(fd_set *rset, fd_set *wset)
     return;
   }
 
-  LOG_INFO("Tun6-handle FD\n");
-
   if(FD_ISSET(tunfd, rset)) {
     size = tun_input(uip_buf, sizeof(uip_buf));
     LOG_DBG("TUN data incoming read:%d\n", size);
-    uip_len = size;
-    tcpip_input();
+    if(size > 0) {
+      uip_len = size;
+      if(pcap_file) {
+        write_pcap_packet(pcap_file);
+      }
+      tcpip_input();
+    }
   }
 }
 
@@ -320,4 +351,169 @@ const struct network_driver tun6_net_driver ={
 };
 
 
+/*---------------------------------------------------------------------------*/
+static int
+tun_callback(const char *optarg)
+{
+  strncpy(config_tundev, optarg, sizeof(config_tundev) - 1);
+  return 0;
+}
+CONTIKI_OPTION(CONTIKI_MIN_INIT_PRIO + 10,
+               {"tun", required_argument, NULL, 't'},
+               tun_callback, "set tun device", "tundev");
+/*---------------------------------------------------------------------------*/
+static int
+pcap_callback(const char *optarg)
+{
+  pcap_output_file = optarg;
+  return 0;
+}
+CONTIKI_OPTION(CONTIKI_MIN_INIT_PRIO + 11,
+               {"pcap", required_argument, NULL, 0},
+               pcap_callback,
+               "save incoming and outgoing IP packets to pcap file",
+               "pcap-output-file");
+/*---------------------------------------------------------------------------*/
+/* TODO PCAP should be moved to another module */
+/*---------------------------------------------------------------------------*/
+#define PCAP_ETHER_TYPE_IPV4 0x0800
+#define PCAP_ETHER_TYPE_ARP  0x0806
+#define PCAP_ETHER_TYPE_IPV6 0x86dd
+
+#define ETH_HEADER_SIZE 14
+#define ETH_CRC32_SIZE 4
+
+// Define the PCAP file header structure
+struct pcap_file_header {
+  uint32_t magic_number;   // Magic number (0xa1b2c3d4)
+  uint16_t version_major;  // Major version (2)
+  uint16_t version_minor;  // Minor version (4)
+  int32_t thiszone;        // Time zone correction (unused)
+  uint32_t sigfigs;        // Accuracy of timestamps (unused)
+  uint32_t snaplen;        // Maximum packet length (65535)
+  uint32_t linktype;       // Link-layer header type (1 for Ethernet)
+};
+
+// Define the PCAP packet header structure
+struct pcap_packet_header {
+  uint32_t ts_sec;         // Timestamp seconds
+  uint32_t ts_usec;        // Timestamp microseconds
+  uint32_t caplen;         // Captured packet length
+  uint32_t len;            // Original packet length
+};
+
+// Define the Ethernet header structure
+struct ethernet_header {
+  uint8_t dest_mac[6];     // Destination MAC address
+  uint8_t src_mac[6];      // Source MAC address
+  uint16_t ether_type;     // EtherType (0x0800 for IPv4, 0x86DD for IPv6)
+};
+/*---------------------------------------------------------------------------*/
+static FILE *
+open_pcap(const char *filename)
+{
+  FILE *pcap_file = fopen(filename, "wb");
+  if(!pcap_file) {
+    return NULL;
+  }
+
+  struct pcap_file_header file_header = {
+    .magic_number = 0xa1b2c3d4,
+    .version_major = 2,
+    .version_minor = 4,
+    .thiszone = 0,
+    .sigfigs = 0,
+    .snaplen = 4096,
+    .linktype = 1  // Ethernet link type
+  };
+
+  fwrite(&file_header, sizeof(file_header), 1, pcap_file);
+  fflush(pcap_file);
+
+  return pcap_file;
+}
+/*---------------------------------------------------------------------------*/
+static void
+log_eth(const uint8_t eth_addr[6])
+{
+  LOG_OUTPUT("%02x", eth_addr[0]);
+  for(int i = 1; i < 6; i++) {
+    LOG_OUTPUT(":%02x", eth_addr[i]);
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+ipv6_to_eth_addr(const uip_ip6addr_t *addr, uint8_t eth_addr[6])
+{
+  const uint8_t *eui64 = &addr->u8[8];
+  /* Reverse the universal/local bit inversion */
+  eth_addr[0] = eui64[0] ^ 0x02;
+  eth_addr[1] = eui64[1];
+  eth_addr[2] = eui64[2];
+  eth_addr[3] = eui64[5];
+  eth_addr[4] = eui64[6];
+  eth_addr[5] = eui64[7];
+}
+/*---------------------------------------------------------------------------*/
+static void
+write_pcap_packet(FILE *pcap_file)
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+
+  if(uip_len + ETH_HEADER_SIZE + ETH_CRC32_SIZE > UIP_BUFSIZE) {
+    LOG_ERR("PCAP: too large IP packet: %u bytes\n", uip_len);
+    return;
+  }
+
+  struct pcap_packet_header packet_header = {
+    .ts_sec = tv.tv_sec,
+    .ts_usec = tv.tv_usec,
+    .caplen = uip_len + ETH_HEADER_SIZE + ETH_CRC32_SIZE,
+    .len = uip_len + ETH_HEADER_SIZE + ETH_CRC32_SIZE
+  };
+
+  struct ethernet_header eth_header = {
+    .dest_mac = {0},
+    .src_mac = {0},
+    .ether_type = UIP_HTONS(PCAP_ETHER_TYPE_IPV6)
+  };
+  ipv6_to_eth_addr(&UIP_IP_BUF->destipaddr, eth_header.dest_mac);
+  ipv6_to_eth_addr(&UIP_IP_BUF->srcipaddr, eth_header.src_mac);
+
+  if(LOG_INFO_ENABLED) {
+    LOG_INFO("PCAP IPv6: ");
+    LOG_INFO_6ADDR(&UIP_IP_BUF->srcipaddr);
+    LOG_INFO_(" (");
+    log_eth(eth_header.src_mac);
+    LOG_INFO_(") to ");
+    LOG_INFO_6ADDR(&UIP_IP_BUF->destipaddr);
+    LOG_INFO_(" (");
+    log_eth(eth_header.dest_mac);
+    LOG_INFO_(") %u bytes\n", uip_len);
+  }
+
+  memmove(&uip_buf[ETH_HEADER_SIZE], uip_buf, uip_len);
+  memcpy(&uip_buf[0], &eth_header, ETH_HEADER_SIZE);
+
+  uint32_t checksum = crc32(uip_buf, uip_len + ETH_HEADER_SIZE);
+  uip_buf[uip_len + ETH_HEADER_SIZE] = checksum & 0xff;
+  uip_buf[uip_len + ETH_HEADER_SIZE + 1] = (checksum >> 8L) & 0xff;
+  uip_buf[uip_len + ETH_HEADER_SIZE + 2] = (checksum >> 16L) & 0xff;
+  uip_buf[uip_len + ETH_HEADER_SIZE + 3] = (checksum >> 24L) & 0xff;
+
+  fwrite(&packet_header, sizeof(packet_header), 1, pcap_file);
+  fwrite(uip_buf, uip_len + ETH_HEADER_SIZE + ETH_CRC32_SIZE, 1, pcap_file);
+  fflush(pcap_file);
+
+  memmove(uip_buf, &uip_buf[ETH_HEADER_SIZE], uip_len);
+}
+/*---------------------------------------------------------------------------*/
+static void
+close_pcap(FILE *pcap_file)
+{
+  if(pcap_file) {
+    fclose(pcap_file);
+  }
+}
 /*---------------------------------------------------------------------------*/
